61227754b2c46b6238766d44ca841ec7cc65876c
Administrator <admin@example.com>
2018-12-09 08:38:16 -0500

    added files

--- pre-image.py
+++ pre-image.py
@@ -15,7 +15,10 @@ def osversion():
     elif OStype == "CentOS":
        print "This is CentOS"
 
+myos = osversion()
+print(myos)
 #osversion()
+"""
 release = ''.join(platform.linux_distribution())
 OS = release.split()
 OStype = OS[0]
@@ -35,18 +38,4 @@ if OStype == "CentOS":
        #print "This is RedHat OS"
 elif OStype == "Red":
                       print "This is RedHat"
-'''
-lines = [ '[rabbitmq_erlang-source]',
-          'name=rabbitmq_erlang-source',
-          'baseurl=https://packagecloud.io/rabbitmq/erlang/el/7/SRPMS',
-          'gpgcheck=1',
-          'enabled=1',
-          'gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-rabbitmq',
-        ]
-
-done = "Operation successfully completed"
-with open("/var/repos.d/test.repo", "w") as f:
-     for i in lines:
-         f.write(i + "\n")
-     print done
-'''
+"""
