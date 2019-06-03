e557a109350d254de6a773d6566749c4cda64bf9
Administrator <admin@example.com>
2019-06-03 07:53:55 -0400

    modified script

--- freeipa_auto.sh
+++ freeipa_auto.sh
@@ -7,12 +7,33 @@
 # Written by J.Niranjan Reddy, DevOps Team#
 ###########################################
 #clear
+HOSTIP=$(hostname -i)
+HOSTNAME=$(hostname -s)
+DOMAIN=$(hostname -d)
+REALM=$(echo ${DOMAIN} | tr '[:lower:]' '[:upper:]')
+LOOKUPIP=$(host ${HOSTNAME} | awk '{print $4}')
+
+# Checking if script is being executed as not-root user,
+# this script will exit if non-root user run this script.
+if [ $EUID -ne 0 ]; then
+   echo ""
+   echo "This script must be run as root" 1>&2
+   echo ""
+   exit 1
+fi
+########################################################
 
 if [ "$1" == "ipa-server" ]; then
 
       echo ""
       echo -e "Checking Pre-flight...."
-      RPMCHK=$(rpm -qa | grep -w ipa-server)
+         if [ "$HOSTIP" == "$LOOKUPIP" ]; then
+               echo -e "DNS Resolution looks good.."
+            else
+               echo -e "\e[1;31mDNS Resolution failed, please fix it then run this script again.\e[0m"
+         fi
+      
+        rpm -qa | grep -w ipa-server
         if [ "$?" == "0" ]; then
               echo "IPA-server is already installed...exiting."
               echo ""
@@ -24,7 +45,7 @@ elif [ "$1" == "ipa-client" ]; then
       
       echo ""
       echo -e "Checking Pre-flight...."
-      RPMCHK=$(rpm -qa | grep -w ipa-client)
+      rpm -qa | grep -w ipa-client
         if [ "$?" == "0" ]; then
               echo "IPA-client is already installed...exiting."
               exit 1
--- freeipa_case.sh
+++ freeipa_case.sh
@@ -7,38 +7,75 @@
 # Written by J.Niranjan Reddy, DevOps Team#
 ###########################################
 #clear
- 
-
-
-
-
-
-if [ "$1" == "ipa-server" ]; then
-
-      echo ""
-      echo -e "Checking Pre-flight...."
-      #RPMCHK=$(rpm -qa | grep -w ipa-server)
-      rpm -qa | grep -w ipa-server
-        if [ "$?" == "0" ]; then
-              echo "IPA-server is already installed...exiting."
-              echo ""
-              exit 1
-              
-        fi
-#######################################################################    
-elif [ "$1" == "ipa-client" ]; then
-      
-      echo ""
-      echo -e "Checking Pre-flight...."
-      #RPMCHK=$(rpm -qa | grep -w ipa-client)
-      rpm -qa | grep -w ipa-client
-        if [ "$?" == "0" ]; then
-              echo "IPA-client is already installed...exiting."
-              exit 1
-        fi
-    else
-         echo ""
-         echo "Usage of this script $0 ipa-server|ipa-client"
-fi     
+HOSTIP=$(hostname -i)
+HOSTNAME=$(hostname -s)
+DOMAIN=$(hostname -d)
+REALM=$(echo ${DOMAIN} | tr '[:lower:]' '[:upper:]')
+LOOKUPIP=$(host ${HOSTNAME} | awk '{print $4}')
+ONESPACE()
+{
 echo ""
+}
+
+DNSLOOKUP()
+           {
+              if [ "$HOSTIP" == "$LOOKUPIP" ]; then
+                     echo -e "\e[1;32mDNS Resolution looks good...\e[0m"
+                 else
+                     echo -e "\e[1;31mDNS Resolution failed, please fix it then run this script again.\e[0m"
+                     exit 1
+              fi
+           }
+
+# Checking if script is being executed as not-root user,
+# this script will exit if non-root user run this script.
+if [ $EUID -ne 0 ]; then
+   echo ""
+   echo "This script must be run as root" 1>&2
+   echo ""
+   exit 1
+fi
+########################################################
+VERIFYSRV=$(rpm -qa | grep -i ipa-server | wc -l)
+VERIFYCLI=$(rpm -qa | grep -i ipa-client | wc -l)
+MYOPTION=$1
+case $MYOPTION in
+    
+     ipa-server)
+                 ONESPACE
+                 echo -e "Checking Pre-flight...."
+                       ONESPACE
+                       DNSLOOKUP
+                       if [ "$VERIFYSRV" ==  "0" ]; then
+                            ONESPACE
+                            echo -e "\e[1;32mgood to proceed.\e[0m"
+                           else
+                               echo -e "\e[1;31mIPA-server already installed...exiting.\e[0m"
+                               ONESPACE
+                               exit 1
+                       fi
+
+                 ;;
+      ipa-client)
+                 ONESPACE
+                 echo -e "Checking Pre-flight...."
+                 ONESPACE
+                 DNSLOOKUP
+                 if [ "$VERIFYCLI" == "0" ]; then
+                      ONESPACE
+                      echo -e "\e[1;32mgood to proceed.\e[0m"
+                   else 
+                      echo -e "\e[1;31mIPA-client already installed...exiting.\e[0m"
+                      ONESPACE
+                      exit 1
+                 fi
+                   ONESPACE
+                 ;;
+               *)
+
+                 ONESPACE 
+                 echo -e "\e[1;31mUsage of this script $0 ipa-server|ipa-client...\e[0m"
+                 ONESPACE
+               ;;
+esac
 #######################################################################
