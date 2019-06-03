f5cbf7240f9700468677e63dec29a9215bc32f72
Administrator <admin@example.com>
2019-06-03 06:13:13 -0400

    added files

--- freeipa_case.sh
+++ freeipa_case.sh
@@ -7,12 +7,18 @@
 # Written by J.Niranjan Reddy, DevOps Team#
 ###########################################
 #clear
+ 
+
+
+
+
 
 if [ "$1" == "ipa-server" ]; then
 
       echo ""
       echo -e "Checking Pre-flight...."
-      RPMCHK=$(rpm -qa | grep -w ipa-server)
+      #RPMCHK=$(rpm -qa | grep -w ipa-server)
+      rpm -qa | grep -w ipa-server
         if [ "$?" == "0" ]; then
               echo "IPA-server is already installed...exiting."
               echo ""
@@ -24,7 +30,8 @@ elif [ "$1" == "ipa-client" ]; then
       
       echo ""
       echo -e "Checking Pre-flight...."
-      RPMCHK=$(rpm -qa | grep -w ipa-client)
+      #RPMCHK=$(rpm -qa | grep -w ipa-client)
+      rpm -qa | grep -w ipa-client
         if [ "$?" == "0" ]; then
               echo "IPA-client is already installed...exiting."
               exit 1
